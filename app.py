fce82e5385ec909eb2fed9550675bdca0070f849
pudding0803 <nakohui160@gmail.com>
2023-05-14 02:07:53 +0800

    add header, footer, and refactor the structure of /templates

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 
 @app.route('/add_row')
 def add_row():
     row = request.args.get('row')
-    return render_template('row.html', row=row)
+    return render_template('components/row.html', row=row)
 
 
 if __name__ == '__main__':
--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#add-button').click(function () {
+    $('#add-button').on('click', () => {
         $.ajax({
             url: '/add_row',
             type: 'GET',
@@ -8,5 +8,21 @@ $(document).ready(function () {
                 $('#ingredients').append(response);
             }
         });
+    });
+
+    $('#ingredients').on('input', '.adjust', function() {
+        const currentInput = $(this);
+        const currentRow = currentInput.closest('div[id^="row-"]');
+        $('#adjust-button').prop('disabled', !currentInput.val());
+        $('#ingredients > div').not(currentRow).each(function() {
+            $(this).find('.adjust').prop('disabled', currentInput.val());
+        });
+    });
+
+    $('#update-button').on('click', () => {
+        const adjust = $('input.adjust').filter(function() {
+            return $(this).val() !== '';
+        }).first().val();
+        console.log(adjust);
     });
 });
